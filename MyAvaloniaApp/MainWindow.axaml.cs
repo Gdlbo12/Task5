27d97ad05a1d8698752b9769973cc4cb534174d8
dmitrijzitov <dmitrijzitov@edu.hse.ru>
2026-03-18 19:20:00 +0800

    Добавлены элементы управления и вывод результата

--- MyAvaloniaApp/MainWindow.axaml.cs
+++ MyAvaloniaApp/MainWindow.axaml.cs
@@ -1,7 +1,11 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace MyAvaloniaApp;
 
 public partial class MainWindow : Window
 {
+    private void Button1_OnClick(object? sender, RoutedEventArgs e)
+    {
+    }
 }
